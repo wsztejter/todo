04f58c96b784214ba21d6a8b5b4b3bee25bbaaad
Wioleta Sztejter <wiola0406@gmail.com>
2023-06-02 15:45:59 +0200

    added components

---
+++ components/Task.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, Stylesheet } from 'react-native';
+
+const Task = () => {
+    return (
+        <View>
+            <Text>Task</Text>
+        </View>
+    )
+}
+
+const styles = Stylesheet.create({
+
+});
+
+export default Task;
